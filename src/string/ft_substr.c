97063f16a42a0d61e26a093913b7ad53627cef18
Victor Brouwer <vbrouwer@f1r3s12.codam.nl>
2023-05-26 16:28:22 +0200

    calloc in substr

--- src/string/ft_substr.c
+++ src/string/ft_substr.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        ::::::::            */
-/*   ft_substr.c                                        :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: vbrouwer <vbrouwer@student.codam.nl>         +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2022/10/11 17:45:45 by vbrouwer      #+#    #+#                 */
-/*   Updated: 2022/10/31 10:51:23 by vbrouwer      ########   odam.nl         */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vbrouwer <vbrouwer@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/11 17:45:45 by vbrouwer          #+#    #+#             */
+/*   Updated: 2023/05/23 13:37:18 by vbrouwer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		return (ft_strdup(""));
 	if (len > s_len)
 		len = s_len;
-	ptr = malloc((len + 1) * sizeof(char));
+	ptr = ft_calloc(len + 1, sizeof(char));
 	if (ptr == NULL)
 		return (NULL);
 	while (len > 0 && s[(start + x)] && start < s_len)
